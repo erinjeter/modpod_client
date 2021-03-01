6cd02edc58e989330b0b7ea485937e25e1cf42b2
Lesley Reuter <lesley.reuter@gmail.com>
2021-03-01 17:56:57 -0500

    working on cards

--- src/components/podcast/Podcasts.js
+++ src/components/podcast/Podcasts.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Podcast from "./Podcast";
 import SearchResult from "./SearchResult";
+import { CardDeck, CardColumns } from 'reactstrap';
 
 const baseURL = "https://listen-api.listennotes.com/api/v2";
 
@@ -31,7 +32,7 @@ const Podcasts = () => {
   };
   const podFetch = () => {
     fetch(
-      `${baseURL}/${endpt}?q=${search}&sort_by_date=${sort_by_date}&offset=0&len_max=${len_max}&genre_ids=${genre_ids}&published_after=${published_after}&only_in=title%2Cdescription&language=${language}&safe_mode=0`,
+      `${baseURL}/${endpt}?q=${search}&type=podcast&sort_by_date=${sort_by_date}&offset=0&len_max=${len_max}&genre_ids=${genre_ids}&published_after=${published_after}&only_in=title%2Cdescription&language=${language}&safe_mode=0`,
       requestOptions
     )
       .then((res) => res.json())
@@ -55,11 +56,16 @@ const Podcasts = () => {
           required
         />
         <br />
+     <span>Published After: </span>
+         <input type="date" name="published_after" pattern="[0-9]{8}" onChange={(e) => setPublished_After(e.target.value)} />
+        <br />
         <button type="submit">Submit</button>
       </form>
+      <CardColumns>
       {podcasts.map(podcast=> (
         <SearchResult podcast={podcast} />
       ))}
+      </CardColumns>
     </div>
   );
 };
--- src/components/podcast/SearchResult.js
+++ src/components/podcast/SearchResult.js
@@ -1,27 +1,54 @@
-import React from 'react';
-import {
-  Card, Button, CardImg, CardTitle, CardText, CardDeck,
-  CardSubtitle, CardBody
-} from 'reactstrap';
+import {Card, CardImg, CardTitle, CardText, CardDeck, CardFooter, CardBody} from 'reactstrap';
+import Button from 'react-bootstrap/Button';
+//import CardDeck from 'react-bootstrap/CardDeck';
+// import { CardColumns, CardDeck } from 'reactstrap';
+import './search.css';
 
-
-const SearchResult = ({ podcast }) => {
-  return (
-    <div>
-       <CardDeck style={{width:"30%", height:"60%"}}  >
-      <Card key={podcast.id} >
-        <CardImg top width="30%" src={podcast.image} alt="Card image cap" />
-        <CardBody >
-          <CardTitle tag="h5">{podcast.title_original}</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-          <CardText>{podcast.description_original}</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-      </CardDeck>
-    </div>
+const SearchResult = ({podcast}) => {
+    return (
+  <>
+  <Card key={podcast.id} id='searchCardCss'>
+    <CardImg variant="top" src={podcast.image} />
+    <CardBody>
+      <CardTitle>{podcast.title_original}</CardTitle>
+      <CardText >
+      {podcast.description_original}
+      </CardText>
+    </CardBody>
+  </Card>
+  
+  </>
   );
 };
 
-
 export default SearchResult;
+
+
+
+// import React from 'react';
+// import {
+//   Card, Button, CardImg, CardTitle, CardText, CardDeck,
+//   CardSubtitle, CardBody
+// } from 'reactstrap';
+
+
+// const SearchResult = ({ podcast }) => {
+//   return (
+//     <div>
+//        <CardDeck style={{width:"30%", height:"60%"}}  >
+//       <Card key={podcast.id} >
+//         <CardImg top width="30%" src={podcast.image} alt="Card image cap" />
+//         <CardBody >
+//           <CardTitle tag="h5">{podcast.title_original}</CardTitle>
+//           <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+//           <CardText>{podcast.description_original}</CardText>
+//           <Button>Button</Button>
+//         </CardBody>
+//       </Card>
+//       </CardDeck>
+//     </div>
+//   );
+// };
+
+
+// export default SearchResult;
