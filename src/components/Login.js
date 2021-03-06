7491579501c4cfb2b2ca5f3a189d97925ae0ddd1
Erin Jeter <ekjeter@gmail.com>
2021-03-06 16:33:20 -0500

    take 3

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Form, FormGroup, Label, Input, Button } from "reactstrap";
-import APIURL from "./helpers/environment";
+import APIURL from "../helpers/environment";
 
 const Login = (props) => {
   const [email, setEmail] = useState("");
