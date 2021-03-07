d09b54009e719b9a2bfdc24c5159260e02ca68ea
Erin Jeter <ekjeter@gmail.com>
2021-03-07 18:39:29 -0500

    stuff

--- src/components/podcast/reviews/ReviewEdit.js
+++ src/components/podcast/reviews/ReviewEdit.js
@@ -1,39 +1,54 @@
-import React, {useState} from 'react';
-import {Button, Form, FormGroup, Label, Input, Modal, ModalHeader, ModalBody} from 'reactstrap';
-
+import React, { useState } from "react";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Modal,
+  ModalHeader,
+  ModalBody,
+} from "reactstrap";
 
 const ReviewEdit = (props) => {
-    const [editReview, setEditReview] = useState(props.podcast.review);    
+  const [editReview, setEditReview] = useState(props.podcast.review);
 
-    const reviewUpdate = (event, review) => {
-        event.preventDefault();
-        fetch(`http://localhost:3000/favorites/${props.podcast.id}`, {
-            method: 'PUT',
-            body: JSON.stringify({favorites: {review: editReview}}),
-            headers: new Headers({
-                'Content-Type': 'application/json',
-                'Authorization': props.token
-            })
-        }) .then((res) => {
-            props.fetchReviews();
-            props.updateOff();
-        })
-    }
+  const reviewUpdate = (event, review) => {
+    event.preventDefault();
+    fetch(`http://localhost:3000/favorites/${props.podcast.id}`, {
+      method: "PUT",
+      body: JSON.stringify({ favorites: { review: editReview } }),
+      headers: new Headers({
+        "Content-Type": "application/json",
+        Authorization: props.token,
+      }),
+    }).then((res) => {
+      props.fetchReviews();
+      props.updateOff();
+    });
+  };
 
-    return(
+  return (
     //    <Modal isOpen={true}>
     //        <ModalHeader>Log a Review</ModalHeader>
     //        <ModalBody>
-               <Form onSubmit={reviewUpdate}>
-                   <FormGroup>
-                       <Label htmlFor="review"> Edit Review:
-                       <Input name="review" value={editReview} onChange={(e) => setEditReview(e.target.value)}/></Label>
-                   </FormGroup>
-                   <Button type="submit">Update the Review</Button>
-               </Form>
+    <Form onSubmit={reviewUpdate}>
+      <FormGroup>
+        <Label htmlFor="review">
+          {" "}
+          Edit Review:
+          <Input
+            name="review"
+            value={editReview}
+            onChange={(e) => setEditReview(e.target.value)}
+          />
+        </Label>
+      </FormGroup>
+      <Button type="submit">Update the Review</Button>
+    </Form>
     //        </ModalBody>
     //    </Modal>
-    )
-}
+  );
+};
 
 export default ReviewEdit;
--- src/components/podcast/reviews/ReviewIndex.js
+++ src/components/podcast/reviews/ReviewIndex.js
@@ -1,57 +1,73 @@
-import React, {useState, useEffect} from 'react';
-import {Container, Row, Col, Label, Button, Form, FormGroup, Input} from 'reactstrap';
-import ReviewCreate from './ReviewCreate';
-import ReviewEdit from './ReviewEdit';
+import React, { useState, useEffect } from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Label,
+  Button,
+  Form,
+  FormGroup,
+  Input,
+} from "reactstrap";
+import ReviewCreate from "./ReviewCreate";
+import ReviewEdit from "./ReviewEdit";
 
 const ReviewIndex = (props) => {
-    const [reviews, setReviews] = useState([]);
-    const [updateActive, setUpdateActive] = useState(false);
-    const [reviewToUpdate, setReviewToUpdate] = useState({});
+  const [reviews, setReviews] = useState([]);
+  const [updateActive, setUpdateActive] = useState(false);
+  const [reviewToUpdate, setReviewToUpdate] = useState({});
 
-    const fetchReviews = () => {
-        fetch(`http://localhost:3000/favorites/1`, {
-            method: "PUT",
-            headers: new Headers({
-            'Content-Type': 'application/json',
-            'Authorization': localStorage.getItem('token')
-        })
-    }) .then( (res) => res.json())
-        .then((json) => {
-            setReviews(json)
-            console.log(json)
-        })
-    }
+  const fetchReviews = () => {
+    fetch(`http://localhost:3000/favorites/1`, {
+      method: "PUT",
+      headers: new Headers({
+        "Content-Type": "application/json",
+        Authorization: localStorage.getItem("token"),
+      }),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        setReviews(json.results);
+      });
+  };
 
-    const editUpdateReview = (review) => {
-        setReviewToUpdate(review);
-        console.log(review);
-    }
+  const editUpdateReview = (review) => {
+    setReviewToUpdate(review);
+    console.log(review);
+  };
 
-    const updateOn = () => {
-        setUpdateActive(true);
-    }
+  const updateOn = () => {
+    setUpdateActive(true);
+  };
 
-    const updateOff = () => {
-        setUpdateActive(false);
-    }
+  const updateOff = () => {
+    setUpdateActive(false);
+  };
 
-    useEffect(() => {
-        fetchReviews();
-    }, [])
-    
-    return(
-       <Container>
-       {/* <ReviewTable reviews={reviews} editUpdateReview={editUpdateReview}
+  useEffect(() => {
+    fetchReviews();
+  }, []);
+
+  return (
+    <Container>
+      {/* <ReviewTable reviews={reviews} editUpdateReview={editUpdateReview}
                    updateOn={updateOn} fetchReviews={fetchReviews} token={props.token}/> */}
-        <Form onSubmit={fetchReviews}>
-            <FormGroup>
-            <Label htmlFor='reviews'>Reviews:</Label>
-            <Input name='reviews' value={reviews} onChange={(e) => setReviews(e.target.value)} />
-            </FormGroup>
-            <Button type='submit' color="success">submit</Button>
-        </Form>
-       </Container>
-    )
-}
+      <Form onSubmit={fetchReviews}>
+        <FormGroup>
+          <Label htmlFor="reviews">Add a review:</Label>
+          <Input
+            name="reviews"
+            value={reviews}
+            onChange={(e) => setReviews(e.target.value)}
+          />
+        </FormGroup>
+        <Button type="submit" color="success" onClick={() => setReviews()}>
+          submit
+        </Button>
+      </Form>
+    </Container>
+  );
+};
 
 export default ReviewIndex;
